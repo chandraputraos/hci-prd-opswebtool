a242075f4d791deb3e73fc2e79cbded95ec54e5f
Chandra <vasilio.vip@gmail.com>
2020-06-01 20:40:43 +0700

    curl method rev

--- resources/js/components/mobile/UnsyncAldi.vue
+++ resources/js/components/mobile/UnsyncAldi.vue
@@ -1,120 +1,200 @@
 <template>
 <div class="row">
+
     <div class="col-6">
 
         <div class="card card-primary">
+
             <div class="card-header">
+
                 <h3 class="card-title">Unsync ALDI</h3>
+
                 <div class="card-tools">
+
                     <button type="button" class="btn btn-block btn-warning btn-sm" data-toggle="modal" data-target="#modal-lg">Wiki</button>
 
                 </div>
+
                 <!-- /.card-header -->
 
             </div>
+
             <div class="card-body">
+
                 <div class="row">
+
                     <div class="col-lg-4">
+
                         <div class="input-group input-group-sm">
+
                             <select class="form-control" v-model='range'>
+
                                 <option value="" selected disabled hidden>Select date</option>
+
                                 <option>Sysdate-1</option>
+
                                 <option>Sysdate-7</option>
+
                             </select>
+
                             <span class="input-group-append">
+
                                 <button type="button" @click='showContract()' class="btn btn-success btn-flat">Go!</button>
+
                             </span>
+
                         </div>
+
                     </div>
+
                 </div>
+
                 <br>
+
                 <table class="table table-bordered">
+
                     <tbody>
+
                         <tr>
+
                             <th style="width: 10px">#</th>
+
                             <th>CONTRACT NUMBER</th>
+
                             <th>STATUS DESC</th>
+
                             <th>CREATED DATE</th>
 
                         </tr>
+
                         <tr v-for="(result, index) in result" :key="result.contract_number">
+
                             <td>{{index}}</td>
+
                             <td>{{ result.CONTRACT_NUMBER }}</td>
+
                             <td>{{ result.STATUS_DESC}}</td>
+
                             <td>{{ result.CREATED_DATE }}</td>
 
                         </tr>
+
                     </tbody>
+
                 </table>
+
             </div>
+
             <!-- /.card-body -->
 
-
-
             <!-- /.MODAL -->
+
             <div class="modal fade" id="modal-lg">
+
                 <div class="modal-dialog modal-lg">
+
                     <div class="modal-content">
+
                         <div class="modal-header">
+
                             <h4 class="modal-title">Section Info</h4>
+
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+
                                 <span aria-hidden="true">&times;</span>
+
                             </button>
+
                         </div>
+
                         <div class="modal-body">
+
                             <p>KBASE</p>
+
                         </div>
+
                         <div class="modal-footer justify-content-between">
+
                             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
 
                         </div>
+
                     </div>
+
                     <!-- /.modal-content -->
+
                 </div>
+
                 <!-- /.modal-dialog -->
+
             </div>
+
             <!-- /.modal -->
 
         </div>
+
     </div>
 
-<div class="col-md-6">
-<div class="card card-warning">
-              <div class="card-header">
+    <div class="col-md-6">
+
+        <div class="card card-warning">
+
+            <div class="card-header">
+
                 <h3 class="card-title">Contract Sync Result</h3>
-              </div>
-              <!-- /.card-header -->
-              <!-- form start -->   
-              <form class="form-horizontal">
-                <div class="card-body">
-                  <table class="table table-hover table-bordered">
-                                <thead>
-                                <tr>
-                                    <th>Result</th>
-                                   
-                                   
-                                </tr>
-                                </thead>
-                                <tbody>
-                                  <tr v-for="user in users" :key="user.Hit">
-                                    <td>{{ users.Hit }}</td>
-                                   
-                                 
-                                   
-                                </tr>
-                                </tbody>
-                            </table>
-                </div>
-                <!-- /.card-body -->
-                <div class="card-footer">
-                 
-                  <button type="button"  @click='executeContract()' class="btn btn-danger btn-sm float-right">Execute!</button>
-                </div>
-                <!-- /.card-footer -->
-              </form>
+
             </div>
-</div>
-    
+
+            <!-- /.card-header -->
+
+            <!-- form start -->
+
+            <form class="form-horizontal">
+
+                <div class="card-body">
+
+                    <table class="table table-hover table-bordered">
+
+                        <thead>
+
+                            <tr>
+
+                                <th>Result</th>
+
+                            </tr>
+
+                        </thead>
+
+                        <tbody>
+
+                            <tr v-for="user in users" :key="user.Hit">
+
+                                <td>{{ users.Hit }}</td>
+
+                            </tr>
+
+                        </tbody>
+
+                    </table>
+
+                </div>
+
+                <!-- /.card-body -->
+
+                <div class="card-footer">
+
+                    <button type="button" @click='executeContract()' class="btn btn-danger btn-sm float-right">Execute!</button>
+
+                </div>
+
+                <!-- /.card-footer -->
+
+            </form>
+
+        </div>
+
+    </div>
+
 </div>
 </template>
 
@@ -125,7 +205,7 @@ export default {
         return {
             result: [],
             range: '',
-            users : [],
+            users: [],
         }
     },
 
@@ -145,22 +225,18 @@ export default {
                     fetch().then().catch(error => this.$vToastify.error(error));
 
                 });
-
         },
 
-         executeContract: function() {
-
-
-            axios.get('http://10.56.45.133/itoperation/laraAPI/mobile/soap_unsync.php', {
-                                   })
+        executeContract: function () {
+            axios.get('http://10.56.45.133/itoperation/laraAPI/mobile/soap_unsync.php', {})
                 .then(response => {
                     this.users = response.data;
-                     this.$vToastify.success("Unsync Data Berhasil");
+                    this.$vToastify.success("Unsync Data Berhasil");
                 })
-                .catch(function(error) {
+                .catch(function (error) {
                     console.log(error);
                 });
-}
+        }
     }
 }
 </script>
--- resources/js/components/pgw/Incoming.vue
+++ resources/js/components/pgw/Incoming.vue
@@ -1,58 +1,58 @@
 <template>
     <div class="row">
         <div class="col-12">
-
-                       <div class="card card-primary">
+    
+            <div class="card card-primary">
                 <div class="card-header">
                     <h3 class="card-title">Incoming Payment</h3>
                     <div class="card-tools">
-      <button type="button" class="btn btn-block btn-warning btn-sm" data-toggle="modal" data-target="#modal-lg">Wiki</button>
-          
-                  
-                 
-                </div>
-                <!-- /.card-header -->
-                
+                        <button type="button" class="btn btn-block btn-warning btn-sm" data-toggle="modal" data-target="#modal-lg">Wiki</button>
+    
+    
+    
+                    </div>
+                    <!-- /.card-header -->
+    
                 </div>
                 <div class="card-body">
-                    <div class="row"> 
-                        <div class="col-lg-2"> 
-                       <div class="input-group">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text">
-                        <i class="fas fa-at"></i>
-                      </span>
-                    </div>
-                 <input type="text" class="form-control form-control-sm"  v-model='partner_id' placeholder="Channel">
-                  </div>              
+                    <div class="row">
+                        <div class="col-lg-2">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                            <i class="fas fa-at"></i>
+                                          </span>
+                                </div>
+                                <input type="text" class="form-control form-control-sm" v-model='partner_id' placeholder="Channel">
+                            </div>
                         </div>
-                        <div >
-                    <div class="input-group">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text">
-                        <i class="far fa-calendar-alt"></i>
-                      </span>
-                    </div>
-                     <datetime format="YYYY-MM-DD H:i:s"  v-model='date'   name='dob2'></datetime>
-                  </div>
+                        <div>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                            <i class="far fa-calendar-alt"></i>
+                                          </span>
+                                </div>
+                                <datetime format="YYYY-MM-DD H:i:s" v-model='date' name='dob2'></datetime>
+                            </div>
                         </div>
-
-
-                         <div  >
-                           <div class="input-group">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text">
-                        <i class="far fa-calendar-alt"></i>
-                      </span>
-                    </div>
-                     <datetime format="YYYY-MM-DD H:i:s"  v-model='date2'   name='dob2'></datetime>
-                  </div>
+    
+    
+                        <div>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                            <i class="far fa-calendar-alt"></i>
+                                          </span>
+                                </div>
+                                <datetime format="YYYY-MM-DD H:i:s" v-model='date2' name='dob2'></datetime>
+                            </div>
                         </div>
-
-
-                         <div class="col-md-1">
-                        <button type="button" @click='showByID()' class="btn btn-sm btn-success">Search.!</button>
-                         </div>
+    
+    
+                        <div class="col-md-1">
+                            <button type="button" @click='showByID()' class="btn btn-sm btn-success">Search.!</button>
+                        </div>
                     </div>
                     <br>
                     <table class="table table-bordered">
@@ -61,94 +61,90 @@
                                 <th style="width: 10px">#</th>
                                 <th>PARTNER_ID</th>
                                 <th>CRE_TMS</th>
-                                <th >CONTRACT_NUMBER</th>
-                                 <th>IS_PROCESSED</th>
-                                  <th>IS_REVERSED</th>
-                                  <th>PAYMENT_TYPE</th>
-                                   
+                                <th>CONTRACT_NUMBER</th>
+                                <th>IS_PROCESSED</th>
+                                <th>IS_REVERSED</th>
+                                <th>PAYMENT_TYPE</th>
+    
                             </tr>
-                           <tr v-for="(pgw, index) in pgw" :key="pgw.cre_tms">
-                             <td>{{index}}</td>
-                                    <td>{{ pgw.PARTNER_ID }}</td>
-                                    <td>{{ pgw.CRE_TMS}}</td>
-                                    <td>{{ pgw.CONTRACT_NUMBER }}</td>
-                                    <td>{{ pgw.IS_PROCESSED }}</td>
-                                    <td>{{ pgw.IS_REVERSED }}</td>
-                                    <td>{{ pgw.PAYMENT_TYPE }}</td>
-                                  
-                                   </tr>
+                            <tr v-for="(pgw, index) in pgw" :key="pgw.cre_tms">
+                                <td>{{index}}</td>
+                                <td>{{ pgw.PARTNER_ID }}</td>
+                                <td>{{ pgw.CRE_TMS}}</td>
+                                <td>{{ pgw.CONTRACT_NUMBER }}</td>
+                                <td>{{ pgw.IS_PROCESSED }}</td>
+                                <td>{{ pgw.IS_REVERSED }}</td>
+                                <td>{{ pgw.PAYMENT_TYPE }}</td>
+    
+                            </tr>
                         </tbody>
                     </table>
                 </div>
                 <!-- /.card-body -->
-
-                 <!-- /.MODAL -->
-             <div class="modal fade" id="modal-lg">
-        <div class="modal-dialog modal-lg">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h4 class="modal-title">Section Info</h4>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-           <p>KBASE</p>
-            </div>
-            <div class="modal-footer justify-content-between">
-              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-             
-            </div>
-          </div>
-          <!-- /.modal-content -->
-        </div>
-        <!-- /.modal-dialog -->
-      </div>
-      <!-- /.modal -->
-
-
+    
+                <!-- /.MODAL -->
+                <div class="modal fade" id="modal-lg">
+                    <div class="modal-dialog modal-lg">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h4 class="modal-title">Section Info</h4>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                  </button>
+                            </div>
+                            <div class="modal-body">
+                                <p>KBASE</p>
+                            </div>
+                            <div class="modal-footer justify-content-between">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+    
+                            </div>
+                        </div>
+                        <!-- /.modal-content -->
+                    </div>
+                    <!-- /.modal-dialog -->
+                </div>
+                <!-- /.modal -->
+    
+    
             </div>
         </div>
     </div>
-
-
 </template>
 
 
 <script>
-
 import datetime from 'vuejs-datetimepicker'
-    export default {
-     components: { datetime },
-        data() {
-            return {
-                pgw: [],
-                partner_id :"",             
-                date :'',
-                date2 :''
-            }
-            
-        },        
- 
-         methods:        
-         {              
-       
-    
-       
+export default {
+    components: { datetime },
+    data() {
+        return {
+            pgw: [],
+            partner_id: "",
+            date: '',
+            date2: ''
+        }
+
+    },
+
+    methods: {
+
+
+
         showByID: function() {
 
 
             axios.get('http://10.56.45.133/itoperation/laraAPI/pgw/incoming.php', {
                     params: {
-                      partner_id: this.partner_id,
+                        partner_id: this.partner_id,
                         date: this.date,
                         date2: this.date2
 
                     }
                 })
                 .then(response => {
-                   this.pgw = response.data;
-                   this.$vToastify.success("Data berhasil diambil");
+                    this.pgw = response.data;
+                    this.$vToastify.success("Data berhasil diambil");
                 })
                 .catch(function(error) {
                     console.log(error.response);
@@ -159,5 +155,5 @@ import datetime from 'vuejs-datetimepicker'
 
         }
     }
-    }
+}
 </script>
